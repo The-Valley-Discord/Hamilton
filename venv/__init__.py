0743531a16eb7eefa8435a2e51a5d9c47592b2d4
MissBaroness <66095541+MissBaroness@users.noreply.github.com>
2020-08-02 14:48:12 -0500

    pre-launch commit

--- venv/__init__.py
+++ venv/__init__.py
@@ -68,6 +68,29 @@ async def bank(ctx, currency, owner):
             await ctx.send(embed=await build_embed('discord log', 'Message', text))
 
 @discordbot.command()
+async def burn(ctx, currency, owner):
+    qualified = False
+    if "Moderators" in [r.name for r in ctx.message.author.roles]:
+        qualified = True
+    if qualified == False:
+        text = str("You don't have the votes.")
+        await ctx.send(embed=await build_embed('discord log', 'Message', text))
+    if qualified == True:
+        currency_id = await get_currency_id(currency)
+        if currency_id != None:
+            is_owner = await check_owner(currency_id[0], owner)
+            if is_owner == True:
+                write = await delete_owner(str(currency_id[0]), owner)
+                if write == True:
+                    text = f"{owner} is no longer a banker for {currency_id[1]}."
+                    await ctx.send(embed=await build_embed('discord log', 'Message', text))
+            elif is_owner == False:
+                text = f"{owner} is not a banker for {currency_id[1]}."
+                await ctx.send(embed=await build_embed('discord log', 'Message', text))
+        else:
+            text = f"{currency} does not exist."
+            await ctx.send(embed=await build_embed('discord log', 'Message', text))
+@discordbot.command()
 async def award(ctx, amount, currency, user):
     currency_id = await get_currency_id(currency)
     if currency_id == None:
@@ -92,11 +115,127 @@ async def award(ctx, amount, currency, user):
                 if write == True:
                     text = f"{user} was awarded {amount} {currency_id[1]}."
                     await ctx.send(embed=await build_embed('discord log', 'Message', text))
+        elif currency_id[3] == 'False':
+            text = str(f"{currency_id[1]} not currently enabled.")
+            await ctx.send(embed=await build_embed('discord log', 'Message', text))
+
+@discordbot.command()
+async def set(ctx, amount, currency, user):
+    currency_id = await get_currency_id(currency)
+    if currency_id == None:
+        text = f"{currency} does not exist."
+        await ctx.send(embed=await build_embed('discord log', 'Message', text))
+    else:
+        if "Moderators" in [r.name for r in ctx.message.author.roles]:
+            qualified = True
+
+        if qualified == False:
+            text = str("You don't have the votes.")
+            await ctx.send(embed=await build_embed('discord log', 'Message', text))
+        if qualified == True:
+                user_object = {
+                    'user_id': user,
+                    'currency_id': str(currency_id[0]),
+                    'total': amount
+                }
+                write = await set_balance(user_object)
+
+                if write == True:
+                    text = f"{user}'s balance for {currency_id[1]} was set to {amount}."
+                    await ctx.send(embed=await build_embed('discord log', 'Message', text))
+
+@discordbot.command()
+async def enable(ctx, currency):
+    qualified = False
+    if "Moderators" in [r.name for r in ctx.message.author.roles]:
+        qualified = True
+    if qualified == False:
+        text = str("You don't have the votes.")
+        await ctx.send(embed=await build_embed('discord log', 'Message', text))
+    if qualified == True:
+        currency_id = await get_currency_id(currency)
+
+        if currency_id != None:
+            currency_object = {
+                'currency_id': str(currency_id[0]),
+                'enabled': "True"
+            }
+            await toggle(currency_object)
+            text = f"{currency_id[1]} has been enabled."
+            await ctx.send(embed=await build_embed('discord log', 'Message', text))
+        else:
+            text = f"{currency} does not exist."
+            await ctx.send(embed=await build_embed('discord log', 'Message', text))
+
+@discordbot.command()
+async def disable(ctx, currency):
+    qualified = False
+    if "Moderators" in [r.name for r in ctx.message.author.roles]:
+        qualified = True
+    if qualified == False:
+        text = str("You don't have the votes.")
+        await ctx.send(embed=await build_embed('discord log', 'Message', text))
+    if qualified == True:
+        currency_id = await get_currency_id(currency)
+
+        if currency_id != None:
+            currency_object = {
+                'currency_id': str(currency_id[0]),
+                'enabled': "False"
+            }
+            await toggle(currency_object)
+            text = f"{currency_id[1]} has been disabled."
+            await ctx.send(embed=await build_embed('discord log', 'Message', text))
+        else:
+            text = f"{currency} does not exist."
+            await ctx.send(embed=await build_embed('discord log', 'Message', text))
+
+@discordbot.command()
+async def score(ctx, currency, user):
+    currency_id = await get_currency_id(currency)
+    if currency_id == None:
+        text = f"{currency} does not exist."
+        await ctx.send(embed=await build_embed('discord log', 'Message', text))
+    else:
+        user_object = {
+            'id': user,
+            'currency_id': str(currency_id[0])
+        }
+        score = await get_score(user_object)
+        if score != None:
+            text = f"{user} has {score[2]} {currency_id[1]}."
+            await ctx.send(embed=await build_embed('discord log', 'Message', text))
+        else:
+            text = f"{user} has zero {currency_id[1]}."
+            await ctx.send(embed=await build_embed('discord log', 'Message', text))
+
+@discordbot.command()
+async def leaderboard(ctx, currency):
+    currency_id = await get_currency_id(currency)
+    if currency_id == None:
+        text = f"{currency} does not exist."
+        await ctx.send(embed=await build_embed('discord log', 'Message', text))
+    else:
+
+        board = await get_leaderboard(str(currency_id[0]))
+        if board != None:
+            await ctx.send(f"Current leaderboard for {currency_id[1]}:")
+            await ctx.send(embed=await build_embed('leaderboard', 'Leaberboard', board))
+        else:
+            text = f"No current totals for {currency_id[1]}"
+            await ctx.send(embed=await build_embed('discord log', 'Message', text))
+
 
 #non-command functions
 async def build_embed(mode, title, data):
     if mode == 'discord log':
         embed = discord.Embed(color=0xD4AF37, description=data)
+
+    if mode == 'leaderboard':
+        text = "\n"
+        for item in data:
+            text+=str(f"{item[0]}:   {item[2]}\n")
+        embed = discord.Embed(color=0xD4AF37, description=text)
 
     return embed
 
@@ -132,6 +271,38 @@ async def write_to_db(table, data):
             conn.close()
             print("The SQLLite connection is closed")
 
+async def set_balance(data):
+    try:
+        conn = await create_connection()
+        conn.execute(
+            "UPDATE users SET total=:total WHERE user_id=:user_id AND currency_id=:currency_id", {"total": data['total'], "user_id": data['user_id'], "currency_id": data['currency_id']}
+        )
+
+        return True
+    except conn.Error as error:
+        print("Failed to write single row to sqlite table", error)
+    finally:
+        if (conn):
+            conn.commit()
+            conn.close()
+            print("The SQLLite connection is closed")
+
+async def toggle(currency):
+    try:
+        conn = await create_connection()
+        conn.execute(
+            "UPDATE currency SET enabled=:enabled WHERE currency_id=:currency_id", {"enabled": currency['enabled'], "currency_id": currency['currency_id']}
+        )
+
+        return True
+    except conn.Error as error:
+        print("Failed to write single row to sqlite table", error)
+    finally:
+        if (conn):
+            conn.commit()
+            conn.close()
+            print("The SQLLite connection is closed")
+
 async def check_row(table, column, data):
     exists = False
     try:
@@ -153,6 +324,21 @@ async def check_row(table, column, data):
             conn.close()
             print("The SQLLite connection is closed")
 
+async def delete_owner(currency_id, owner_id):
+    try:
+        conn = await create_connection()
+        record = conn.execute(
+            f"DELETE FROM owners WHERE currency_id=:currency_id AND owner_id=:owner_id", {"currency_id": currency_id, "owner_id": owner_id}
+        ).fetchone()
+        return True
+    except conn.Error as error:
+        print("Failed to delete single row to sqlite table", error)
+    finally:
+        if (conn):
+            conn.commit()
+            conn.close()
+            print("The SQLLite connection is closed")
+
 async def get_currency_id(value):
     try:
         conn = await create_connection()
@@ -167,6 +353,40 @@ async def get_currency_id(value):
         return record
     except conn.Error as error:
         print("Failed to write single row to sqlite table", error)
+    finally:
+        if (conn):
+            conn.commit()
+            conn.close()
+            print("The SQLLite connection is closed")
+
+async def get_score(user):
+    try:
+        conn = await create_connection()
+        record = conn.execute(
+            "SELECT * FROM users WHERE user_id=:id AND currency_id=currency_id", {"id": user['id'], "currency_id": user['currency_id']}
+        ).fetchone()
+
+        return record
+    except conn.Error as error:
+        print("Failed to read single row to sqlite table", error)
+    finally:
+        if (conn):
+            conn.commit()
+            conn.close()
+            print("The SQLLite connection is closed")
+
+async def get_leaderboard(currency_id):
+    try:
+        conn = await create_connection()
+        records = conn.execute(
+            "SELECT * FROM users WHERE currency_id=currency_id", {"currency_id": currency_id}
+        ).fetchall()
+
+        sorted_records = sorted(records, key=lambda x: x[2], reverse=True)
+
+        return sorted_records
+    except conn.Error as error:
+        print("Failed to read single row to sqlite table", error)
     finally:
         if (conn):
             conn.commit()
